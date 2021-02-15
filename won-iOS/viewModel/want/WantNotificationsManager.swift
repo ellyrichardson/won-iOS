eedc4bd91517cc2ae8ac50a9f526e8bb63c06172
darkestmidnight <elly.richardson18@gmail.com>
2021-02-14 20:05:53 -0600

    completed base notification system

--- won-iOS/viewModel/want/WantNotificationsManager.swift
+++ won-iOS/viewModel/want/WantNotificationsManager.swift
@@ -11,17 +11,27 @@ import UserNotifications
 class WantNotificationsManager {
     static func scheduleNotificationFromWant(want: Want) {
         let content = UNMutableNotificationContent()
-        content.title = want.getName()
-        content.subtitle = want.getName() + " is due in " + String(want.getDaysLeft()) + " days."
+        //content.title = want.getName()
+        content.subtitle = want.getName() + " is due in " + String(want.getDaysLeft()) + " days"
         content.sound = UNNotificationSound.default
-        
-        // show this notification five seconds from now
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 65, repeats: (want.getNotification()?.isRepeating())!)
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: retrieveDateComponentForWantDaysLeft(daysLeft: want.getDaysLeft()), repeats: (want.getNotification()?.isRepeating())!)
         
         let request = UNNotificationRequest(identifier: want.getId(), content: content, trigger: trigger)
         
         // add our notification request
         UNUserNotificationCenter.current().add(request)
+    }
+    
+    private static func retrieveDateComponentForWantDaysLeft(daysLeft: Int) -> DateComponents {
+        let wantDueDate = DateUtils.addDayToDate(date: Date(), days: Double(daysLeft))
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.month = calendar.component(.month, from: wantDueDate)
+        dateComponents.day = calendar.component(.day, from: wantDueDate)
+        dateComponents.hour = calendar.component(.hour, from: wantDueDate)
+        dateComponents.year = calendar.component(.year, from: wantDueDate)
+        return dateComponents
     }
     
     // Removes notification based on the target Want
