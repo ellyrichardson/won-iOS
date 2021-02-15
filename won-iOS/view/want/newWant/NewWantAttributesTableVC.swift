7449cf97ad5fd7469764d153690f254b720f5f7a
darkestmidnight <elly.richardson18@gmail.com>
2021-02-15 01:14:56 -0600

    added some final touches for the notification system

--- won-iOS/view/want/newWant/NewWantAttributesTableVC.swift
+++ won-iOS/view/want/newWant/NewWantAttributesTableVC.swift
@@ -14,8 +14,6 @@ class NewWantAttributesTableVC: UITableViewController, UIImagePickerControllerDe
     private let SEGUE_TO_NOTIFICATIONS = "newWantNotificationsVCSegue"
     private let UNWIND_TO_ATTRIBUTES_VC = "unwindToNewWantAttributesTableVC"
     
-    //private var checkButtonEnabled = false
-    
     typealias EditedTextHandler = (_ name: String) -> Void
     typealias EditedImageHandler = (_ name: UIImage) -> Void
     typealias IsCheckButtonEnabledHandler = (_ name: Bool) -> Void
@@ -36,9 +34,7 @@ class NewWantAttributesTableVC: UITableViewController, UIImagePickerControllerDe
     var didEditWantNotification: (EditedNotificationHandler)?
     
     private var wantNotification = WantNotificationViewModel()
-    // This changes based on the NewWantNotesVC value
-    private var wantNotes = ""
-    //private var notificationEnabledIndicator = true
+    private var wantNotes = "" // This changes based on the NewWantNotesVC value
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,16 +42,10 @@ class NewWantAttributesTableVC: UITableViewController, UIImagePickerControllerDe
         wantInterestPointsField.addTarget(self, action: #selector(self.wantInterestPointsFieldDidChange(_:)), for: .editingChanged)
         
         prepareImage()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     func prepareImage() {
-        wantImage.layer.cornerRadius = 190//wantImage.frame.size.width/2.1
+        wantImage.layer.cornerRadius = 190
         wantImage.clipsToBounds = true
         wantImage.layer.borderWidth = 2.0
         wantImage.layer.borderColor = UIColor.white.cgColor
@@ -94,36 +84,16 @@ class NewWantAttributesTableVC: UITableViewController, UIImagePickerControllerDe
         if !self.wantNameField.text!.isEmpty && !self.wantInterestPointsField.text!.isEmpty {
             return true
         }
-        return false//self.checkButtonEnabled
+        return false
     }
-    
-    /*
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
-        let text = (textField.text! as NSString).replacingCharacters(in: range, with: string)
-
-        if !text.isEmpty{
-            self.checkButtonEnabled = true
-        } else {
-            self.checkButtonEnabled = false
-        }
-        return true
-    }*/
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
         wantImage.addGestureRecognizer(tap)
         wantImage.isUserInteractionEnabled = true
-        /*
-        if indexPath.row == 3 || indexPath.row == 4 || indexPath.row == 5 || indexPath.row == 8 || indexPath.row == 9 || indexPath.row == 10 {
-            // To remove line separator from tableViewCells
-            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
-            cell.directionalLayoutMargins = .zero
-        }*/
     }
     
     @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
-        // handling code
         wantNameField.resignFirstResponder()
         wantInterestPointsField.resignFirstResponder()
         let imagePickerController = UIImagePickerController()
@@ -137,26 +107,15 @@ class NewWantAttributesTableVC: UITableViewController, UIImagePickerControllerDe
         super.prepare(for: segue, sender: sender)
         if (segue.identifier == SEGUE_TO_NOTES) {
             let notesVC = segue.destination as! NewWantNotesVC
-            //let destNavCtrl = segue.destination as! UINavigationController
-            //let notesVC = destNavCtrl.topViewController as! NewWantNotesVC
             notesVC.setDelegate(delegate: self)
             notesVC.setWantNotes(notes: wantNotes)
         }
         if (segue.identifier == SEGUE_TO_NOTIFICATIONS) {
             let notifVC = segue.destination as! NewWantNotificationsVC
-            //let destNavCtrl = segue.destination as! UINavigationController
-            //let notesVC = destNavCtrl.topViewController as! NewWantNotesVC
             notifVC.setDelegate(delegate: self)
             notifVC.setWantNotificationViewModel(wantNotifViewModel: self.wantNotification)
         }
     }
-    
-    /*
-    @IBAction func unwindToHere( _ seg: UIStoryboardSegue) {
-        if let sourceVC = seg.source as? NewWantNotesVC {
-            didEditWantNotes!(sourceVC.getWantNotes())
-        }
-    }*/
     
     func passData(data: Any) {
         if let passedData = data as? String {
