bd6c6883ecaff39bd6e37eb8a6e8f532ec76be09
darkestmidnight <elly.richardson18@gmail.com>
2021-03-08 21:00:16 -0600

    added initial bdd tests

--- won-iOS/viewModel/want/WantManager.swift
+++ won-iOS/viewModel/want/WantManager.swift
@@ -15,7 +15,7 @@ class WantManager {
         let wantViewModels = dataAccess.findAllWants()
         for wantViewModel in wantViewModels {
             let wantDueDate = DateUtils.addDayToDate(date: wantViewModel.getDateCreated(), days: 30.0)
-            print("Want Days Left Checker - Running - Checking " + wantViewModel.getName())
+            print("Want Days Left Checker is Running - Checking " + wantViewModel.getName())
             let expectedDaysLeft = DateUtils.daysBetweenTwoDates(earlyDate: Date(), laterDate: wantDueDate)
             if wantViewModel.getDaysLeft() != expectedDaysLeft && wantViewModel.getDaysLeft() > 0 {
                 dataAccess.updateDaysLeftAsViewModel(viewModel: wantViewModel, daysLeft: expectedDaysLeft)
--- won-iOSTests/ViewModelTests/WantManagerSpec.swift
+++ won-iOSTests/ViewModelTests/WantManagerSpec.swift
@@ -8,10 +8,30 @@
 
 import Quick
 import Nimble
+import RealmSwift
 @testable import won_iOS
 
 class WantManagerSpec: QuickSpec {
     override func spec() {
         
+        describe("Want Manager") {
+            context("days left assigner") {
+                
+                beforeSuite {
+                    Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+                }
+                
+                beforeEach {
+                    let realm = try! Realm()
+                    try! realm.write {
+                        realm.deleteAll()
+                    }
+                }
+                
+                it("should return 30 days") {
+                    
+                }
+            }
+        }
     }
 }
